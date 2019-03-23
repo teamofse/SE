dfb293bdc77635247cd83c04aa936c36ed518e82
dtw <923604040@qq.com>
2019-03-23 18:26:00 +0800

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import home from '../components/home'
 import login from '../components/login'
+import list from '../components/list'
 import App from '../App'
 
 Vue.use(Router)
@@ -13,8 +15,16 @@ export default new Router({
       component: App,
       children: [
         {
+          path: '/home',
+          component: home
+        },
+        {
           path: '/login',
           component: login
+        },
+        {
+          path: '/list',
+          component: list
         }
       ]
     }
