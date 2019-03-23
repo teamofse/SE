0a1557af10b3ae21d9d66229994b886916a72a19
chenyuyuan <1695215206@qq.com>
2019-03-23 22:27:36 +0800

    iteminfo

--- frontend/src/components/ItemInfo.vue
+++ frontend/src/components/ItemInfo.vue
@@ -1,143 +1,177 @@
-<template>
+<template id="itembar0">
   <div class="container">
     <div class="row clearfix">
       <div class="col-md-12 column">
-        <div class="row clearfix">
-          <div class="col-md-2 column">
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
           </div>
-          <div class="col-md-8 column">
-            <nav class="navbar navbar-default" role="navigation">
-              <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
-              </div>
 
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                  <li class="active">
-                    <a href="#">Link</a>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active">
+                <a href="#">Link</a>
+              </li>
+              <li>
+                <a href="#">Link</a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a href="#">Action</a>
                   </li>
                   <li>
-                    <a href="#">Link</a>
+                    <a href="#">Another action</a>
                   </li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-                    <ul class="dropdown-menu">
-                      <li>
-                        <a href="#">Action</a>
-                      </li>
-                      <li>
-                        <a href="#">Another action</a>
-                      </li>
-                      <li>
-                        <a href="#">Something else here</a>
-                      </li>
-                      <li class="divider">
-                      </li>
-                      <li>
-                        <a href="#">Separated link</a>
-                      </li>
-                      <li class="divider">
-                      </li>
-                      <li>
-                        <a href="#">One more separated link</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-                <form class="navbar-form navbar-left" role="search">
-                  <div class="form-group">
-                    <input type="text" class="form-control" />
-                  </div> <button type="submit" class="btn btn-default">Submit</button>
-                </form>
-                <ul class="nav navbar-nav navbar-right">
                   <li>
-                    <a href="#">Link</a>
+                    <a href="#">Something else here</a>
                   </li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-                    <ul class="dropdown-menu">
-                      <li>
-                        <a href="#">Action</a>
-                      </li>
-                      <li>
-                        <a href="#">Another action</a>
-                      </li>
-                      <li>
-                        <a href="#">Something else here</a>
-                      </li>
-                      <li class="divider">
-                      </li>
-                      <li>
-                        <a href="#">Separated link</a>
-                      </li>
-                    </ul>
+                  <li class="divider">
+                  </li>
+                  <li>
+                    <a href="#">Separated link</a>
+                  </li>
+                  <li class="divider">
+                  </li>
+                  <li>
+                    <a href="#">One more separated link</a>
                   </li>
                 </ul>
-              </div>
+              </li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" />
+              </div> <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li>
+                <a href="#">Link</a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a href="#">Action</a>
+                  </li>
+                  <li>
+                    <a href="#">Another action</a>
+                  </li>
+                  <li>
+                    <a href="#">Something else here</a>
+                  </li>
+                  <li class="divider">
+                  </li>
+                  <li>
+                    <a href="#">Separated link</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
 
-            </nav>
+        </nav>
+        <div class="row clearfix">
+          <div class="col-md-1 column">
+          </div>
+          <div class="col-md-10 column">
             <div class="row clearfix">
               <div class="col-md-6 column">
-                <div class="carousel slide" id="carousel-276770">
+                <div class="carousel slide" id="carousel-760586">
                   <ol class="carousel-indicators">
-                    <li class="active" data-slide-to="0" data-target="#carousel-276770">
+                    <li class="active" data-slide-to="0" data-target="#carousel-760586">
                     </li>
-                    <li data-slide-to="1" data-target="#carousel-276770">
+                    <li data-slide-to="1" data-target="#carousel-760586">
                     </li>
-                    <li data-slide-to="2" data-target="#carousel-276770">
+                    <li data-slide-to="2" data-target="#carousel-760586">
                     </li>
                   </ol>
                   <div class="carousel-inner">
                     <div class="item active">
-                      <img alt="" src="v3/default.jpg" />
+                      <img alt="" src="/static/img/ww1.jpg" />
                       <div class="carousel-caption">
                         <h4>
                           First Thumbnail label
                         </h4>
                         <p>
-                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
                         </p>
                       </div>
                     </div>
                     <div class="item">
-                      <img alt="" src="v3/default1.jpg" />
+                      <img alt="" src="/static/img/ww2.jpg" />
                       <div class="carousel-caption">
                         <h4>
                           Second Thumbnail label
                         </h4>
                         <p>
-                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
                         </p>
                       </div>
                     </div>
                     <div class="item">
-                      <img alt="" src="v3/default2.jpg" />
+                      <img alt="" src="/static/img/ww3.jpg" />
                       <div class="carousel-caption">
                         <h4>
                           Third Thumbnail label
                         </h4>
                         <p>
-                          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
                         </p>
                       </div>
                     </div>
-                  </div> <a class="left carousel-control" href="#carousel-276770" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-276770" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                  </div> <a class="left carousel-control" href="#carousel-760586" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-760586" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
                 </div>
               </div>
               <div class="col-md-6 column">
-                <a id="modal-249609" href="#modal-container-249609" role="button" class="btn" data-toggle="modal">触发遮罩窗体</a>
-
-                <div class="modal fade" id="modal-container-249609" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <h2 style="float: left;display: inline-block;clear:left;">标题标题标题</h2>
+                <div style="width: 100%;float: left;clear:left;">
+                  <span style="float: left;display: inline-block;">介绍</span>
+                  <span style="float: left;display: inline-block">:</span>
+                  <p>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
+                    哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
+                    哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
+                    哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
+                    哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
+                  </p>
+                </div>
+                <div style="float: left;clear:left;">
+                  <span style="float: left;">价格</span>
+                  <span style="float: left;">:</span>
+                  <span>99.99</span>
+                </div>
+                <div style="float: left;clear:left;">
+                  <a id="modal-17668" style="float: left;clear: left;margin-left: 0" href="#modal-container-17668" role="button" class="btn" data-toggle="modal">购买</a>
+                  <img src="/static/icon/like.png">
+                </div>
+                <div class="modal fade" id="modal-container-17668" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                   <div class="modal-dialog">
                     <div class="modal-content">
                       <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                         <h4 class="modal-title" id="myModalLabel">
-                          标题
+                          请输入
                         </h4>
                       </div>
                       <div class="modal-body">
-                        内容...
+                        <form class="form-horizontal" role="form">
+                          <div class="form-group">
+                            <div class="col-sm-1">
+                            </div>
+                            <div class="col-sm-10">
+                              <input type="email" class="form-control" id="inputAddress" placeholder="地址"/>
+                            </div>
+                            <div class="col-sm-1">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <div class="col-sm-1">
+                            </div>
+                            <div class="col-sm-10">
+                              <input type="password" class="form-control" id="inputItem" placeholder="商品"/>
+                            </div>
+                            <div class="col-sm-1">
+                            </div>
+                          </div>
+                        </form>
                       </div>
                       <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button> <button type="button" class="btn btn-primary">保存</button>
@@ -151,7 +185,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md-2 column">
+          <div class="col-md-1 column">
           </div>
         </div>
       </div>
