334d61a6dcdb4b486f4108356801627cfc55b59b
dtw <923604040@qq.com>
2019-03-27 21:30:28 +0800

    goods表增删改查

---
+++ backend/src/main/java/com/se/mapper/GoodsMapper.java
@@ -0,0 +1,29 @@
+package com.se.mapper;
+
+import com.se.entity.Goods;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+public interface GoodsMapper {
+
+    //查询所有商品信息
+    @Select("SELECT * FROM goods")
+    List<Goods> queryGoodsById();
+
+    //根据名字查询商品信息
+    @Select("SELECT * FROM goods WHERE title = #{title}")
+    List<Goods> queryGoodsByTitle(@Param("title") String title);
+
+    //插入商品信息
+    @Insert("INSERT INTO goods(id, title, star, name, city) VALUES(#{id}, #{title}, #{star}, #{name}, #{city})")
+    void insertGoods(@Param("id") int id, @Param("title") String title, @Param("star") int star, @Param("name") String name, @Param("city") String city);
+
+    //根据id修改商品信息
+    @Update("Update goods set title = #{title}, star = #{star}, name = #{name}, city = #{city} WHERE id = #{id}")
+    void updateGoods(@Param("id") int id, @Param("title") String title, @Param("star") int star, @Param("name") String name, @Param("city") String city);
+
+    //根据id删除商品信息
+    @Delete("DELETE from goods WHERE id = #{id}")
+    void deleteGoods(@Param("id") int id);
+}
---
+++ backend/src/main/java/com/se/service/GoodsService.java
@@ -0,0 +1,40 @@
+package com.se.service;
+
+
+import com.se.entity.Goods;
+import com.se.mapper.GoodsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GoodsService {
+    @Autowired
+    private GoodsMapper goodsMapper;
+
+    //查询所有商品信息
+    public List<Goods> queryByIdService() {
+        return goodsMapper.queryGoodsById();
+    }
+
+    //根据名字查询商品信息
+    public List<Goods> queryByTitleService(String title) {
+        return goodsMapper.queryGoodsByTitle(title);
+    }
+
+    //插入商品信息
+    public void insertService(int id, String title, int star, String name, String city) {
+        goodsMapper.insertGoods(id, title, star, name, city);
+    }
+
+    //根据id修改商品信息
+    public void updateService(int id, String title, int star, String name, String city) {
+        goodsMapper.updateGoods(id, title, star, name, city);
+    }
+
+    //根据id删除商品信息
+    public void deleteService(int id) {
+        goodsMapper.deleteGoods(id);
+    }
+}
