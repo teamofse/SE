195054e0ed2280e88add95822d41f2e0f5c0a8d6
1037562613@qq.com <1037562613@qq.com>
2019-04-01 18:13:25 +0800

    Revert "Revert "rer""
    This reverts commit 9391e87cc74c23abcb82b82324a74ae96e82cdec.

--- backend/src/main/java/com/se/mapper/UsersMapper.java
+++ backend/src/main/java/com/se/mapper/UsersMapper.java
@@ -2,6 +2,7 @@ package com.se.mapper;
 
 import com.se.entity.Test;
 import com.se.entity.Users;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,4 +13,7 @@ public interface UsersMapper {
 
     @Select("SELECT * FROM users WHERE account = #{account} and password=#{password}")
     Users filterByAccountAndPassword(@Param("account") String account, @Param("password") String password);
+
+    @Insert("INSERT INTO users (account,password,user_tel) VALUES (#{account},#{password},#{telephone});")
+    Users insertUserInfoIntoUsers(@Param("account") String account,@Param("password") String password,@Param("telephone") String telephone);
 }
