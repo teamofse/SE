10c165022aa0ce94f6c31598789c8d4d695c79f6
Astory <dracomalfoy233@163.com>
2019-04-28 16:38:32 +0800

    footer

---
+++ frontend/src/components/footer.vue
@@ -0,0 +1,52 @@
+<template id="footer">
+  <!--<div class="container">-->
+    <div class="row clearfix foot">
+      <div class="col-md-12 column">
+        <div class="row clearfix">
+          <div class="col-md-6 column">
+            <div class="content">
+            <h3>Link</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6 column">
+            <div class="content">
+            <h3>
+              隔空享物
+            </h3>
+            <p>
+              It’s a Vector Icon Management & Communication Platform made by Alimama MUX, designers could upload vector icons here, and users could download it by several kinds of format.Also, the icons could be translated into Fonts for front-end engineers’ usage.
+              ©ALIMAMA MUX, powered by alimama THX.Legal Notices
+            </p>
+            <p>
+              Copyright belongs to the author and original site, please indicate it if you would reproduce the original content, and if you would use it as commercial purpose, please contact us before: zy99945@alibaba-inc.com
+            </p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!--</div>-->
+</template>
+
+<script>
+    export default {
+        name: 'footer'
+    }
+</script>
+
+<style scoped>
+  h3,p{
+    text-align: left;
+  }
+  .foot{
+    height: 270px;
+    background: #06060b;
+    color: #60606d;
+  }
+  .content{
+    margin: 30px;
+  }
+</style>
