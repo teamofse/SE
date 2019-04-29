6588e6ec11264732512f5c485c8fea57b3be3c63
chenyuyuan <1695215206@qq.com>
2019-04-30 01:19:14 +0800

    可显示图片
    示例:<img src="/api/img/2.jpg"/>

--- backend/src/main/java/com/se/controller/ImageController.java
+++ backend/src/main/java/com/se/controller/ImageController.java
@@ -2,10 +2,7 @@ package com.se.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -18,10 +15,10 @@ import java.io.IOException;
 public class ImageController {
     @Value("${prop.upload-folder}")
     private String UPLOAD_FOLDER;
-    @RequestMapping(value = "/img/{imgname}",produces = "get")
+    @RequestMapping(value = "/img/{imgname}",method= RequestMethod.GET,produces = MediaType.IMAGE_JPEG_VALUE)
     @ResponseBody
     public BufferedImage getImage(@PathVariable String imgname) throws IOException {
-
+        System.out.println(imgname);
         return ImageIO.read(new FileInputStream(new File(UPLOAD_FOLDER + "/static/img/" + imgname)));
     }
 
--- frontend/src/components/itemInfo.vue
+++ frontend/src/components/itemInfo.vue
@@ -102,7 +102,6 @@
                       <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button> <button type="button" class="btn btn-primary">保存</button>
                       </div>
-                      <img src="/2.jpg"/>
                     </div>
 
                   </div>
